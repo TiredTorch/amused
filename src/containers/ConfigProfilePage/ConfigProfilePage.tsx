11ee3a74dba3c0360db83ffa8d65ca7a4d3e2af7
Viktor Ivanov <vitochek337@gmail.com>
2023-09-11 19:24:11 +0300

    Configurated change profile page

--- src/containers/ConfigProfilePage/ConfigProfilePage.tsx
+++ src/containers/ConfigProfilePage/ConfigProfilePage.tsx
@@ -1,7 +1,26 @@
 'use client'
 
+import { EditProfileForm } from "@components"
+
 export const ConfigProfilePage = () => {
   return (
-    <div>ConfigProfilePage</div>
+    <div
+      className="flex p-4 py-8 max-md:p-1 h-[calc(100%-60px)]"
+    >
+      <div
+        className="h-full w-full p-4 pt-8 flex justify-start items-start bg-light rounded-xl max-md:rounded-none max-md:p-1 overflow-auto"
+      >
+        <EditProfileForm 
+          onSubmit={console.log} 
+          initialValues={{
+            login: "",
+            password: "",
+            image: undefined,
+            userName: "",
+            description: ""
+          }}
+        />
+      </div>
+    </div>
   )
 }
